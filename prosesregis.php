b95ed71bdb19f54ea739af4dd03447f2cf64b08b
sherli yualinda <42725403+sherliyualinda@users.noreply.github.com>
2018-10-01 10:03:48 +0700

    Update prosesregis.php

--- prosesregis.php
+++ prosesregis.php
@@ -1,7 +1,77 @@
 <?php
+session_start();
+	if (!isset($_SESSION['data'])) {
+		$data= array();
+	}else{
+		$data =$_SESSION['data'];
+	}
+	if (isset($_POST['submit'])) {
+		$nama = $_POST['nama'];
+		$nim = $_POST['nim'];
+		$email = $_POST['email'];
 
-
-
-
-
+		$data_baru = array(
+						'nama' => $nama,
+						'nim' => $nim,
+						'email' => $email
+						);
+		array_push($data, $data_baru);
+	$_SESSION['data']= $data;
+	}
 ?>
+<br>
+<br>
+<br>
+
+<center><table>
+	<tr>
+		<th>NAMA</th>
+	</tr>
+	<?php 
+	$data =$_SESSION['data'];
+	$num =0;
+	while ($num<count($data)) {
+	 ?>
+	<tr>
+		<td><?php echo $data[$num]['nama'];  ?></td>
+	</tr>
+	<?php  
+	$num++;
+}
+	?>
+
+	<tr>
+		<th>NIM</th>
+	</tr>
+	<?php 
+	$data =$_SESSION['data'];
+	$num =0;
+	while ($num<count($data)) {
+	 ?>
+	<tr>
+		<td><?php echo $data[$num]['nim'];  ?></td>
+	</tr>
+	<?php  
+	$num++;
+}
+	?>
+
+	<tr>
+		<th>EMAIL</th>
+	</tr>
+	<?php 
+	$data =$_SESSION['data'];
+	$num =0;
+	while ($num<count($data)) {
+	 ?>
+	<tr>
+		<td><?php echo $data[$num]['email'];  ?></td>
+	</tr>
+	
+	<?php  
+	$num++;
+}
+	?>
+</table>
+<br>
+<a href="registrasi.php">Tambah Data</a>
